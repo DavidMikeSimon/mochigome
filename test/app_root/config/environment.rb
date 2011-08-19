8e3d54b1cfd336311b8cad5eaf200301eb54444b
David Mike Simon <david.mike.simon@gmail.com>
2011-08-19 13:21:52 -0500

    Removing old hobo test mode code

--- test/app_root/config/environment.rb
+++ test/app_root/config/environment.rb
@@ -5,14 +5,6 @@ Rails::Initializer.run do |config|
   config.whiny_nils = true
   config.action_controller.session = {:key => 'rails_session', :secret => 'd229e4d22437432705ab3985d4d246'}
 
-  if ENV['HOBO_TEST_MODE']
-    puts "Loading Hobo gem"
-    config.gem 'hobo'
-    HOBO_TEST_MODE = true
-  else
-    HOBO_TEST_MODE = false
-  end
-  
   if ENV['PSQL_TEST_MODE']
     puts "Using postgresql for a test database"
     config.database_configuration_file = "#{RAILS_ROOT}/config/database-pg.yml"
