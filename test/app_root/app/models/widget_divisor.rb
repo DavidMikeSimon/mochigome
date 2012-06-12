27828cb4a7b79e73cead3ed0f456068f014bf90f
David Mike Simon <david.mike.simon@gmail.com>
2012-06-12 14:25:35 -0400

    Updated widget divisor custom assoc definiton to deal with mysql's untruncating division

--- test/app_root/app/models/widget_divisor.rb
+++ test/app_root/app/models/widget_divisor.rb
@@ -3,8 +3,10 @@ class WidgetDivisor < ActiveRecord::Base
     f.name lambda {|r| "Divisor #{r.divisor}"}
     f.custom_association Widget, lambda {|src_tbl,tgt_tbl|
       # Argh, arel doesn't provide easy access to the modulus operator
-      ((tgt_tbl[:number]/src_tbl[:divisor])*src_tbl[:divisor]).
-        eq(tgt_tbl[:number])
+      (src_tbl[:divisor]*Arel::Nodes::NamedFunction.new(
+        "ROUND",
+        [(tgt_tbl[:number]/src_tbl[:divisor]) - 0.5]
+      )).eq(tgt_tbl[:number])
     }
   end
 
